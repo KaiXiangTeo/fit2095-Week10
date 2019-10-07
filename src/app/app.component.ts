522f2c4af7e569c1dd5e9f4962c744aae1331244
Teo Kai Xiang <dennis_teo@dyn-118-138-35-78.its.monash.edu.au>
2019-10-08 08:46:45 +1100

    week10 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movieAng';
+  section = 0;
+
+  changeSection(sectionId) {
+    this.section = sectionId;
+  }
 }
