a81548cc284c5179e34078df8994c6bff8a57a34
Teo Kai Xiang <dennis_teo@dyn-118-138-35-78.its.monash.edu.au>
2019-10-07 17:30:34 +1100

    week 10

---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from "../database.service";
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css']
+})
+export class MovieComponent implements OnInit {
+  movieDB: any[] = [];
+  section = 1;
+
+  title: string = "";
+  year: number = 0;
+  movieId: string = "";
+
+
+  constructor(private dbService: DatabaseService) { }
+
+  onGetMovies(){
+    this.dbService.getMovies().subscribe((data: any[]) => {
+      this.movieDB = data;
+    })
+  }
+ 
+  OnSaveMovie(item){
+    let obj = {title:this.title, year:this.year};
+    this.dbService.createMovie(obj).subscribe(result => {
+      this.onGetMovies();
+    });
+  }
+
+  //Delete Movie
+  onDeleteMovie(item){
+    this.dbService.deleteMovie(item._id).subscribe(result => {
+      this.onGetMovies();
+    })
+  }
+
+  onDeletebeforeYear(aYear:number){
+    let movies = this.movieDB.filter(element => element.year <= aYear)
+    movies.forEach(element => {
+      this.dbService.deleteMovie(element._id).subscribe()
+    })
+  }
+
+  // This lifecycle callback function will be invoked with the component get initialized by Angular.
+  ngOnInit() {
+    this.onGetMovies();
+  }
+
+  changeSection(sectionId) {
+    this.section = sectionId;
+    this.resetValues();
+  }
+
+  resetValues() {
+    this.title = "";
+    this.year = 0;
+    this.movieId = "";
+  }
+
+
+}
